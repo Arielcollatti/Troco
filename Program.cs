b9bd7bb982960aae69e8d1abca8de30ad9a434c9
Rick Anjo <colatti2017@gmail.com>
2023-03-31 22:03:06 -0300

    exercicioresolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("-Troco-");
+
+decimal ValorCobrar;
+decimal ValorPago;
+decimal ValorTroco;
+
+Console.Write("Valor da Compra R$:");
+ValorCobrar = Convert.ToDecimal(Console.ReadLine())!;
+
+Console.Write("Valor Pago R$:");
+ValorPago = Convert.ToDecimal(Console.ReadLine())!;
+
+ValorTroco = ValorPago - ValorCobrar;
+
+Console.WriteLine($"\ntroco: {ValorTroco:c}");
